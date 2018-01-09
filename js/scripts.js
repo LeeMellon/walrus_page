7ee028ad45a0aec5bd29cf9a781adaa046d9f0e9
Ian Goodrich and Johnny Mayer <ian@>
2018-01-09 10:23:41 -0800

    Add final revision, including html cleanup, js cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,14 @@ $(function(){
     $("#walrus-hidden").toggle();
   });
 
-$(function(){
   $(".fadeable").click(function() {
     $("#walrus-fadeIn").fadeToggle();
     $("#walrus-fadeOut").fadeToggle();
   });
-});
 
-$(function() {
   $(".slideable").click(function() {
     $("#walrus-slideDown").slideToggle();
     $("#walrus-slideUp").slideToggle();
   });
-});
 
 });
